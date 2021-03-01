869e94f059745b9ccb5421767e625d50060cc1ea
hcl <1401859664@qq.com>
2021-03-01 16:37:32 +0800

    first

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('fetch', (event) => {
+    console.log(event);
+    event.respondWith(
+        fetch(event.request).catch(err=>{
+            return caches.match('./offline.html')
+        })
+    )
+})
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('main-cache', (cache) => {
+            console.log('ok');
+            return cache.add('./offline.html')
+        })
+    )
+});
